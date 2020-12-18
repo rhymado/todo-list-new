be6d1b5dfe3fc4d5533698b69e7695dd26175fc6
Fakhri <fakhridho95@gmail.com>
2020-12-18 13:22:23 +0700

    new function master

--- src/pages/newFeature.js
+++ src/pages/newFeature.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function newFeature() {
+  const callFunction = (a) => {
+    return a + 1;
+  };
   return (
     <div>
       <h2>Welcome to the new Feature</h2>
+      <h5>{JSON.stringify(callFunction(3))}</h5>
     </div>
   );
 }
